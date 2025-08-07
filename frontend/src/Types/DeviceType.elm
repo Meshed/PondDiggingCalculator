7f0e4f5df506e32605c1ffd6cb5b04c8b4193f45
Mark Brown <marksbrown@gmail.com>
2025-08-07 04:34:16 -0400

    Test pre-commit hook with good formatting

--- frontend/src/Types/DeviceType.elm
+++ frontend/src/Types/DeviceType.elm
@@ -6,19 +6,18 @@ module Types.DeviceType exposing (DeviceType(..), fromWindowSize)
 
 -}
 
-
 -- DEVICE TYPE
 
 
 {-| Device type enumeration based on screen breakpoints
 Mobile: < 768px
-Tablet: 768px - 1024px  
+Tablet: 768px - 1024px
 Desktop: > 1024px
 -}
 type DeviceType
-    = Mobile    -- < 768px
-    | Tablet    -- 768px - 1024px  
-    | Desktop   -- > 1024px
+    = Mobile -- < 768px
+    | Tablet -- 768px - 1024px
+    | Desktop -- > 1024px
 
 
 {-| Determine device type from window dimensions
@@ -27,7 +26,9 @@ fromWindowSize : { width : Int, height : Int } -> DeviceType
 fromWindowSize { width } =
     if width < 768 then
         Mobile
+
     else if width <= 1024 then
         Tablet
+
     else
         Desktop
